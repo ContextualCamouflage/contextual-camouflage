ac1dcfbc1428280fab2a06a19abaa2e3b06acfce
danigirl329 <danigreaves@gmail.com>
2017-07-27 21:25:19 -0400

    Issue #9 - laying the groundwork for cookie submission

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module ContextualCamoflage
   class Application < Rails::Application
     # TODO add nodejs buildpack to heroku
     # heroku bulidpacks:add 'https://github.com/heroku/heroku-buildpack-nodejs.git'
-    config.asset.paths << Rails.root.join('node_modules')
+    config.assets.paths << Rails.root.join('node_modules')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
