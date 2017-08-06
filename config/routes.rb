23d7c83030310bd13ba9d05815fcb175fc55bd3f
Brittany Martin <brittany.jill.martin@gmail.com>
2017-08-06 16:03:16 -0400

    Issue 10 - Backend - Setup Resource Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
   devise_for :users
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :installations, only: [:index, :show]
   resources :submissions, only: [:create, :index]
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :illnesses, only: [:index, :show]
   
   resources :researches, only: [:create]
+  resources :resources, only: [:index, :show]
   
   root 'installations#index'
 
