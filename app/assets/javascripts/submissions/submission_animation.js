7f2f5b078d45b204ee7cdb485cdc45dc703a83e1
danigirl329 <danigreaves@gmail.com>
2017-08-07 21:28:18 -0400

    Fixing more form stuff.
    Setting the proper local on the top level form
    Setting up functions to control the form ajax submit
    Updates the checkboxes to use the rails helper

--- app/assets/javascripts/submissions/submission_animation.js
+++ app/assets/javascripts/submissions/submission_animation.js
@@ -1,24 +1,13 @@
 $(document).ready(function() {
   animateToAnecdote();
+  submitForm();
 });
-
-// function animateToAnecdote() {
-//   $("[data-next-form]").bind("click", function(e, evt) {
-//     e.preventDefault();
-//     var $el = $(evt.currentTarget).attr($("data-next-form"));
-//     $el.show();
-//     $('html,body,.submission-form.anecdote').animate({
-//   scrollTop: $el.offset().top - ( $(window).height() - $el.outerHeight(true) ) / 2
-// }, 200);
-//   });
-// }
 
 function animateToAnecdote() {
   var current = "research";
     $("[data-next-form]").bind('click', function (evt) {
         var el = $(evt.currentTarget).attr('data-next-form');
         if (el === current) return;
-
 
         var $el = $("." + el);
         var $cur = $("#" + current);
@@ -30,8 +19,20 @@ function animateToAnecdote() {
         $($el).animate({
           scrollTop: $el.offset().top - ( $(window).height() - $el.outerHeight(true) ) / 2
         }, 200);
-
-        // $cur.removeClass('active');
-        // $el.addClass('active');
     });
 }
+
+function submitForm(form) {
+  $("[data-this-form]").bind("click", function(evt) {
+    var formName = $(evt.currentTarget).attr("data-this-form");
+    var form = $("#new_" + formName);
+    // submitAjax(form);
+  });
+}
+
+// function submitAjax(form) {
+//   var formUrl = form.attr("action");
+//   $.ajax({
+//     url: formUrl
+//   });
+// }
