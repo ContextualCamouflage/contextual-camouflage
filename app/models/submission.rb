0026c17844a1da0ad0e55634a6b759881c9ce124
Brittany Martin <brittany.jill.martin@gmail.com>
2017-08-08 20:50:35 -0400

    Re-enable Geocoding and vary the coordinates of the submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,6 +3,7 @@ class Submission < ApplicationRecord
   has_many :anecdotes
   has_many :researches
   validate :double_submit_same_illness
+  before_save :geocode_location
 
   def geocode_location
     location = Geocoder.search(self.ip_address, ip_address: true).first
@@ -18,8 +19,8 @@ private
   def set_location(location)
     self.locality = location.data["city"]
     self.zip_code = location.data["zip_code"]
-    self.latitude = location.data["latitude"]
-    self.longitude = location.data["longitude"]
+    self.latitude = location.data["latitude"] + (Random.new.rand(0.000...0.0007) * [1,-1][rand(2)])
+    self.longitude = location.data["longitude"] + (Random.new.rand(0.000...0.0007) * [1,-1][rand(2)])
   end
 
   def double_submit_same_illness
@@ -27,4 +28,5 @@ private
       errors.add(:cookie, "User already submitted this illness.")
     end
   end
+  
 end
