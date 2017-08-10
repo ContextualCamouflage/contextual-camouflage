3ad95cec70caef43c07f8077e05900c76c47d6fc
danigirl329 <danigreaves@gmail.com>
2017-08-09 22:02:15 -0400

    Issue #10 - Re-adding the singular anecdote for passing test Added a todo for @wonderwomaninthemaking to address (possibly needs a test) changed params[:id] to @resource.id

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,7 +5,8 @@ class ResourcesController < ApplicationController
 
   def show
     @resource = Illness.find_by_id(params[:id])
-    @anecdotes = Anecdote.all.includes(:submission).where(submissions: {illness_id: params[:id]}).order("RANDOM()")
-    # @anecdote = Anecdote.includes(:submission).where(submissions: {illness_id: @resource.id}).order("RANDOM()").first
+    # TODO @brittany this probably needs a test
+    @anecdotes = Anecdote.all.includes(:submission).where(submissions: {illness_id: @resource.id}).order("RANDOM()")
+    @anecdote = Anecdote.includes(:submission).where(submissions: {illness_id: @resource.id}).order("RANDOM()").first
   end
 end
