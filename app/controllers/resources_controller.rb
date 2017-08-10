a726ad8e9ec800bb75b7fc82652b2b213b58d3eb
danigirl329 <danigreaves@gmail.com>
2017-08-09 22:18:56 -0400

    Fixing reek error

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,9 +4,13 @@ class ResourcesController < ApplicationController
   end
 
   def show
-    @resource = Illness.find_by_id(params[:id])
+    @resource = find_all_resources(params[:id])
     # TODO @brittany this probably needs a test
     @anecdotes = Anecdote.all.includes(:submission).where(submissions: {illness_id: @resource.id}).order("RANDOM()")
     # @anecdote = Anecdote.includes(:submission).where(submissions: {illness_id: @resource.id}).order("RANDOM()").first
+  end
+
+  def find_all_resources(id)
+    @resource ||= Illness.find_by_id(id)
   end
 end
