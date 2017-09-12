8660332684b2e9141df320a8ccfb0e6204f974e0
Brittany Martin <brittany.jill.martin@gmail.com>
2017-09-12 19:56:16 -0400

    Issue #31 - Make a Tool for Adding Installation with Locality Information

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,3 +1,12 @@
 class Installation < ApplicationRecord
   has_many :submissions, :primary_key => :locality, :foreign_key => :locality
+  after_create :set_locality
+
+  private
+
+  def set_locality
+    map = Geocoder.search([self.latitude, self.longitude])
+    self.locality = map.first.data["address_components"].third["long_name"] if map.present?
+    self.save!
+  end
 end
