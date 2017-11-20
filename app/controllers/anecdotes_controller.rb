082701990124bd00c9c534c6d29234ff06ebf07a
danigirl329 <danigreaves@gmail.com>
2017-11-19 19:33:40 -0500

    Issue #11 - Form Submission
    Changed text_area to text_area_tag to return a string
    Updated labels to target input ids so clicking on them should be easier.

--- app/controllers/anecdotes_controller.rb
+++ app/controllers/anecdotes_controller.rb
@@ -1,9 +1,7 @@
 class AnecdotesController < ApplicationController
 
   def create
-    #TODO: why am I getting an array from the Frontend? Can this be a string field?
-    #I'd like to remove the .first on the next line
-    anecdote = params[:anecdote].first
+    anecdote = params[:anecdote]
     submission = Submission.find_by_cookie(cookies[:cc]).id
     @anecdote  = Anecdote.create(body: anecdote, submission_id: submission)
     @anecdote.save!
