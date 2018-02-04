f25c23b452bf79227702358762420f3ce286af14
danigirl329 <danigreaves@gmail.com>
2018-02-03 22:28:49 -0500

    Closes Issue #39 - Display View
    Creating the main display view
    Added a new route
    Added random stories and associated illness ids with them
    Fixed spacing on larger screens

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -9,4 +9,11 @@ class InstallationsController < ApplicationController
     @illnesses = Illness.all
   end
 
+  def main
+    @installation = Installation.find_by_id(params[:id])
+    @submissions = @installation.submissions
+    @illnesses = Illness.all
+    @anecdotes = Anecdote.includes(:submission).order("RANDOM()").first(5)
+  end
+
 end
